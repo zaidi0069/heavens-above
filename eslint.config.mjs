0eb0d9f04e3fbd2d005f3d20753532721e5e688b
Muhammad Zaid <zayyed.0069@gmail.com>
2024-04-24 04:28:13 +0500

    updated tests

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,7 +1,7 @@
 import globals from "globals";
-import pluginJs from "@eslint/js";
+
 
 export default [
-  { files: ["**/*.js"], languageOptions: { sourceType: "commonjs", globals: globals.browser } },
-  pluginJs.configs.recommended,
+  {files: ["**/*.js"], languageOptions: {sourceType: "commonjs"}},
+  {languageOptions: { globals: globals.browser }},
 ];
